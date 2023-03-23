6d5fce5e02e02c77e419a4e7f45a1535857eaefa
lucianodarosa <luciano-darosa@hotmail.com>
2023-03-23 18:38:24 -0300

    Add project files.

---
+++ MinhasTarefasAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MinhasTarefasAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
